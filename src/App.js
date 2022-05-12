b6613d157d5696e2213a83efbddf76a7fd9dc58b
LorellasDad <LorellasDad@gmail.com>
2022-05-12 10:55:38 +0100

    that

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V21</h1>
+        <h1>Hello from V212</h1>
       </header>
     </div>
   );
